08276047500d9cfca859a7882317f9900f193bcb
NicolaDiSante <nickymacha90@gmail.com>
2021-04-13 22:18:34 +0200

    Snack 2 completato

---
+++ Jsnack2/script.js
@@ -0,0 +1,7 @@
+var number = parseInt(prompt("Inserisci un numero"));
+
+if (number%2 === 0){
+  console.log("Questo è un numero pari: " + number)
+}else if (number%2 !== 0){
+  console.log(number + 1);
+}
